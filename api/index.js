e79430b5416c065b557d3300a64a438202cff54b
Pk22012003 <punyakakkar34@gmail.com>
2024-07-08 13:13:47 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB!");
@@ -25,3 +29,4 @@ app.get('/tes',(req,res) => {
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
